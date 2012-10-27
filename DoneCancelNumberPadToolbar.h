247d3e9c7d7a448c8d3a3e7a5d845cabc0759939
broderboy <timothy.broder@gmail.com>
2012-10-27 12:58:12 -0300

    Update DoneCancelNumberPadToolbar.h

--- DoneCancelNumberPadToolbar.h
+++ DoneCancelNumberPadToolbar.h
@@ -1,5 +1,5 @@
 //
-//  ApplyCancelNumberPadUIToolbar.h
+//  DoneCancelNumberPadToolbar.h
 //
 //  Created by Timothy Broder on 8/17/12.
 //  Based on http://stackoverflow.com/a/11382326/647343 by Luda
